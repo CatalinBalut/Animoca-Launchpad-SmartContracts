8559d08afdc4d1c45864e88e085373aa20f3ceef
CatalinBalut <balut.catalin@yahoo.com>
2022-09-28 12:17:10 +0300

    added PayoutWallet to SaleFacet

--- contracts/SaleFacet.sol
+++ contracts/SaleFacet.sol
@@ -1,18 +1,22 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import {ContractOwnershipBase} from "@animoca/ethereum-contracts/contracts/access/base/ContractOwnershipBase.sol";
 import {ContractOwnershipStorage} from "@animoca/ethereum-contracts/contracts/access/libraries/ContractOwnershipStorage.sol";
 
-contract SaleFacet is ContractOwnershipBase {
+import {PayoutWalletBase} from "@animoca/ethereum-contracts/contracts/payment/base/PayoutWalletBase.sol";
+import {PayoutWalletStorage} from "@animoca/ethereum-contracts/contracts/payment/libraries/PayoutWalletStorage.sol";
+
+contract SaleFacet is ContractOwnershipBase, PayoutWalletBase {
     using ContractOwnershipStorage for ContractOwnershipStorage.Layout;
+    using PayoutWalletStorage for PayoutWalletStorage.Layout;
 
     constructor(){
     }
 
     function initContract() public {
          ContractOwnershipStorage.layout().proxyInit(msg.sender);
-
+         PayoutWalletStorage.layout().proxyInit(payable(msg.sender));
     } 
 
 }
